8546e8c29ec9a8a2af34fed992f33a12901ff824
saikrishnadev95 <saikrishnatsk35@gmail.com>
2023-11-09 20:56:15 +0530

    changes

---
+++ 04-functions.sh
@@ -0,0 +1,11 @@
+# If we assign a name to commands , then it is a function
+
+# Declare a function
+abc() {
+  echo Hello World
+  echo Hello sai
+  echo Hello all
+}
+
+# how to access function
+abc
