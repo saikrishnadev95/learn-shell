dda9a90996c1f789b7767d20e245fa461e9f3ea0
saikrishnadev95 <saikrishnatsk35@gmail.com>
2023-11-10 01:24:08 +0530

    changes

--- 02-print.sh
+++ 02-print.sh
@@ -1,6 +1,13 @@
 echo Hello World
 
 # There are 6 colors in total in shell
+# Red         - 31
+# Green       - 32
+# Yellow      - 33
+# Blue        - 34
+# Magenta     - 35
+# Cyan        - 36
+
 # Red         - 41
 # Green       - 42
 # Yellow      - 43
@@ -8,15 +15,22 @@ echo Hello World
 # Magenta     - 45
 # Cyan        - 46
 
+# Red         - 51
+# Green       - 52
+# Yellow      - 53
+# Blue        - 54
+# Magenta     - 55
+# Cyan        - 56
+
 # Syntax of Color print
 # echo -e "\e[COLmMESSAGE\[0m"
 # -e option to enable color
 # \e[COLM - To defined which color
 # \e[0m -  To disable color
 
-echo -e "\e[41mRED COLOR\e[0m"
-echo -e "\e[42mGREEN COLOR\e[0m"
-echo -e "\e[43mYELLOW COLOR\e[0m"
-echo -e "\e[44mBLUE COLOR\e[0m"
-echo -e "\e[45mMAGENTA COLOR\e[0m"
-echo -e "\e[46mCYAN COLOR\e[0m"
+echo -e "\e[31mRED COLOR\e[0m"
+echo -e "\e[32mGREEN COLOR\e[0m"
+echo -e "\e[33mYELLOW COLOR\e[0m"
+echo -e "\e[34mBLUE COLOR\e[0m"
+echo -e "\e[35mMAGENTA COLOR\e[0m"
+echo -e "\e[36mCYAN COLOR\e[0m"
