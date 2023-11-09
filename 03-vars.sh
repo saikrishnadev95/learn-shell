06de3901bb30bf8299787479425bd6718b67c10c
saikrishnadev95 <saikrishnatsk35@gmail.com>
2023-11-09 14:53:51 +0530

    for changes

---
+++ 03-vars.sh
@@ -0,0 +1,13 @@
+# If we name some data it is called variable
+
+# syntax to declare variable
+# var_name=data
+
+
+a=10
+x=100
+y=true
+name=DevOps
+
+## How to access variablle
+echo value of a = $a
